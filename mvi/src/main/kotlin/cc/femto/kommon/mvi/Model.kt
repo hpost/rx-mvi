40e800530a94f680513460d9667ca1294aff1d17
Henning Post <henningpost@gmail.com>
2019-10-03 15:18:50 -0700

    Rename Intent to Action
    This avoids confusion with the Intent concept on Android and more
    closely follows the naming of Redux.

--- mvi/src/main/kotlin/cc/femto/kommon/mvi/Model.kt
+++ mvi/src/main/kotlin/cc/femto/kommon/mvi/Model.kt
@@ -2,25 +2,25 @@ package cc.femto.kommon.mvi
 
 import io.reactivex.Observable
 
-interface Model<INTENT : Intent, STATE> {
+interface Model<ACTION : Action, STATE> {
     /**
-     * Attach the model to an intent stream
+     * Attach the model to an action stream
      *
-     * NB: Always attach model to view before attaching intent stream to model
+     * NB: Always attach model to view before attaching action stream to model
      *
      * Usage:
      * <code>
      *     override fun onCreate(savedInstanceState: Bundle?) {
      *         super.onCreate(savedInstanceState)
      *         view.attach(model.state().observeOn(AndroidSchedulers.mainThread()))
-     *         model.attach(view.intents())
+     *         model.attach(view.actions())
      *     }
      * </code>
      */
-    fun attach(intents: Observable<INTENT>)
+    fun attach(actions: Observable<ACTION>)
 
     /**
-     * Detach the model from previously attached intent stream
+     * Detach the model from previously attached action stream
      *
      * Usage:
      * <code>
