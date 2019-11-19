9c8cd2a8fb4c67d1fd38e4871f1207876be72e1d
Henning Post <henningpost@gmail.com>
2019-11-18 16:38:54 -0800

    Add `attachComponent` utility function
    Most helpful for nested components that optionally receive
    additional actions from the parent component.

---
+++ rx-mvi/src/main/kotlin/cc/femto/mvi/Util.kt
@@ -0,0 +1,15 @@
+package cc.femto.mvi
+
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+
+fun <ACTION : Action, STATE, T : ACTION> attachComponent(
+    view: View<ACTION, STATE>,
+    model: Model<ACTION, STATE>,
+    actions: Observable<T> = Observable.empty(),
+    block: () -> Unit = {}
+) {
+    view.attach(model.state().observeOn(AndroidSchedulers.mainThread()))
+    model.attach(view.actions().mergeWith(actions))
+    block()
+}
