4a83cbea1848ba06a1d5871218037011c66586e0
Henning Post <henningpost@gmail.com>
2020-05-21 17:08:38 -0700

    Attach actions to model before attaching state to view
    This allows parent components to listen for state from nested
    components that emit immediately after being attached.
    If a nested component is attached before the parent component's
    model is operational those early state changes can be missed.

--- rx-mvi/src/main/kotlin/cc/femto/mvi/Model.kt
+++ rx-mvi/src/main/kotlin/cc/femto/mvi/Model.kt
@@ -6,14 +6,12 @@ interface Model<ACTION : Action, STATE> {
     /**
      * Attach the model to an action stream
      *
-     * NB: Always attach model to view before attaching action stream to model
-     *
      * Usage:
      * <code>
      *     override fun onCreate(savedInstanceState: Bundle?) {
      *         super.onCreate(savedInstanceState)
-     *         view.attach(model.state().observeOn(AndroidSchedulers.mainThread()))
      *         model.attach(view.actions())
+     *         view.attach(model.state().observeOn(AndroidSchedulers.mainThread()))
      *     }
      * </code>
      */
